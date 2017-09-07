270de23f89fe325f6b938d7ca743988f24f7c62c
faith-muli <faithmuli33@gmail.com>
2017-09-07 11:50:00 +0300

    meteor app

---
+++ client/main.js
@@ -0,0 +1,52 @@
+import { Template } from 'meteor/templating';
+import { Notes } from     '../lib/collections.js';
+import  { Accounts} from 'meteor/accounts-base';
+
+//accounts config
+Accounts.ui.config({
+    passwordSignupFields:'USERNAME_ONLY'
+})
+
+import './main.html';
+
+Template.body.helpers
+({
+    notes(){
+        return Notes.find({});
+    },
+
+});
+
+Template.add.events({
+    'submit.add-form':function () {
+        event.preventDefault();
+
+         //getting input value
+        const target = event.target;
+        const text   = target.text.value;
+
+        //insert note into collection
+        /*Notes.insert({
+            text,
+            createdAt: new Date(),
+            owner: Meteor.userId(),
+            username: Meteor.user().username
+        });*/
+
+        Meteor.call('notes.insert', text)
+        //clear form
+        target.text.value = '';
+        //close modal
+        $('#addModal').modal('close');
+
+        return false;
+    }
+});
+Template.note.events({
+    'click .delete-note':function () {
+        /*Notes.remove(this._id);*/
+        Meteor.call('notes.remove', this);
+
+    }
+});
+
